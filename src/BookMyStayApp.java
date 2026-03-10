5bb42cbefbb0df294339946d95f9193538490405
TanishqYadav174 <ty8099@srmist.edu.in>
2026-03-10 14:40:42 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+/**
+ * Book My Stay - Hotel Booking Management System
+ * Version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("=================================");
+
+    }
+}
